46b921a1cabbbe7e9f8f2fe95e7376f5a0171565
Jemmee Yung <jemmee@gmail.com>
2026-01-16 18:37:44 -0800

    eat: Update implementation in C to demonstrate the use of POSIX thread (pthread).

--- pthread_test.c
+++ pthread_test.c
@@ -4,21 +4,59 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void *print_message(void *ptr) {
-  printf("Hello from the thread!\n");
-  return NULL;
+#define NUM_THREADS 4
+
+// Shared data
+int global_counter = 0;
+pthread_mutex_t counter_mutex;
+
+// The function the threads will execute
+void *perform_work(void *thread_id) {
+  long id = (long)thread_id;
+
+  for (int i = 0; i < 3; i++) {
+    // Lock the mutex before touching shared data
+    pthread_mutex_lock(&counter_mutex);
+
+    global_counter++;
+    printf("Thread #%ld updated counter to: %d\n", id, global_counter);
+
+    // Unlock so other threads can use it
+    pthread_mutex_unlock(&counter_mutex);
+
+    // Sleep briefly to simulate work and allow thread interleaving
+    usleep(100000);
+  }
+
+  pthread_exit(NULL);
 }
 
 int main() {
-  pthread_t thread1;
+  pthread_t threads[NUM_THREADS];
+  pthread_mutex_init(&counter_mutex, NULL);
 
-  // Create the thread
-  pthread_create(&thread1, NULL, print_message, NULL);
+  printf("Starting %d threads...\n", NUM_THREADS);
 
-  // Wait for the thread to finish
-  pthread_join(thread1, NULL);
+  // Create threads
+  for (long t = 0; t < NUM_THREADS; t++) {
+    int rc = pthread_create(&threads[t], NULL, perform_work, (void *)t);
+    if (rc) {
+      printf("Error: return code from pthread_create() is %d\n", rc);
+      exit(-1);
+    }
+  }
 
-  printf("Thread finished. Back in main.\n");
+  // Wait for all threads to finish (joining)
+  for (int t = 0; t < NUM_THREADS; t++) {
+    pthread_join(threads[t], NULL);
+  }
+
+  printf("All threads complete. Final counter value: %d\n", global_counter);
+
+  // Clean up
+  pthread_mutex_destroy(&counter_mutex);
   return 0;
 }
