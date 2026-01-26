6069449ed34ab0f52bee2e3a983ea6b6b8c82e66
Jemmee Yung <jemmee@gmail.com>
2026-01-26 11:11:16 -0800

    feat: Add implementation of bubble sort in Java.

--- BubbleSortTest.java
+++ BubbleSortTest.java
@@ -5,7 +5,7 @@
 public class BubbleSortTest {
     public static int[] bubbleSort(int[] array) {
         if (array != null) {
-            for (int i = 0; i < array.length; i++) {
+            for (int i = 0; i < array.length - 1; i++) {
                 for (int j = 0; j < array.length - i - 1; j++) {
                     if (array[j] > array[j + 1]) {
                         int temp = array[j];
