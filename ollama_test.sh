1776aabda36c4c4c2ebaea62c6a24f68bd6bf42d
Jemmee Yung <jemmee@gmail.com>
2026-06-16 20:41:44 -0700

    feat: Add implementation in shell script and Python to demonstrate the use of Ollama for an interactive chatbot.

---
+++ ollama_test.sh
@@ -0,0 +1,11 @@
+brew install ollama
+
+brew services start ollama
+
+ollama --version
+
+ollama pull gemma2:2b
+
+ollama list
+
+ollama run gemma2:2b "Why is the sky blue? Answer in exactly 1 sentence."
