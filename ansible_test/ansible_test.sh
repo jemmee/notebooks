7c754034bff5e167fd3a1536bbf169db863513cb
Jemmee Yung <jemmee@gmail.com>
2026-06-23 12:29:23 -0700

    feat: Add implementation to demonstrate the use of Ansible for Configuration Management (CM) and IT automation to streamline application/system deployment and maintenance.

---
+++ ansible_test/ansible_test.sh
@@ -0,0 +1,9 @@
+brew install ansible
+
+ansible --version
+
+ansible-playbook -i inventory.ini setup_development_box.yml
+
+ansible-playbook -i inventory.ini setup_development_box.yml
+
+more ~/ansible_sandbox/config.env
